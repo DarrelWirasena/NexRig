5f97d344b70f8f76783abf78abfff35b9cd406f0
bagusstyyy7-pixel <bagusstyyy7@gmail.com>
2026-02-06 08:58:21 +0700

    push1

--- database/migrations/2026_02_05_095006_create_categories_table.php
+++ database/migrations/2026_02_05_095006_create_categories_table.php
@@ -9,19 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+ public function up(): void
+{
+    Schema::create('categories', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->string('slug')->unique(); // Untuk URL SEO friendly
+        $table->timestamps();
+    });
+}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('categories');
-    }
+public function down(): void
+{
+    Schema::dropIfExists('categories');
+}
 };
