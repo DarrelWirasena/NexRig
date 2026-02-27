f2bad7d20b449ebfa52bb0c048a16e1568365032
DarrelWirasena <benedictusdarrel9@gmail.com>
2026-02-27 11:30:36 +0700

    memindahkan panggilan app.js pada layout/app agar tidak blink saat buka page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html class="dark" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    {{-- 2. ASSETS (Vite) --}}
+    {{-- Memanggil app.css dan app.js yang sudah kita bersihkan --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -169,10 +172,6 @@
         });
     });
     </script>
-
-    {{-- 2. ASSETS (Vite) --}}
-    {{-- Memanggil app.css dan app.js yang sudah kita bersihkan --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     {{-- Stack Scripts (Untuk script halaman spesifik) --}}
     @stack('scripts')
